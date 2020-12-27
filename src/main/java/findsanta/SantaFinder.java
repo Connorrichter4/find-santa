dd816848437dc41cd954f37d806266bd9e1702c5
Connor Richter <Connorrichter4@gmail.com>
2020-12-27 14:41:49 -0600

    added santa suspects

--- src/main/java/findsanta/SantaFinder.java
+++ src/main/java/findsanta/SantaFinder.java
@@ -3,8 +3,24 @@ package findsanta;
 public class SantaFinder {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		
+		SantaSuspects santaSuspects = new SantaSuspects(10);
+		santaSuspects.addSuspects("Bob Bezanga");
+		santaSuspects.addSuspects("Sally Zimmers");
+		santaSuspects.addSuspects("Peter Pinkelton");
+		santaSuspects.addSuspects("Bobby Drop Tables");
+		santaSuspects.addSuspects("Rudolph Rednose");
+		santaSuspects.addSuspects("Bob Ross");
+		santaSuspects.addSuspects("Ubiga Altman");
+		santaSuspects.addSuspects("Tony Ross");
+		santaSuspects.addSuspects("Aaron Rogers");
+		santaSuspects.addSuspects("Yolo Once");
+		santaSuspects.addSuspects("Yoka Yamalla");
+		santaSuspects.addSuspects("Chris Cringle");
+		santaSuspects.addSuspects("Doug Dimmadome");
+		
+		santaSuspects.printSuspects();
+		
 	}
 
 }
